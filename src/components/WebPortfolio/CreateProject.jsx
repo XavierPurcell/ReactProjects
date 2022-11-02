e345fb2129e1352d0764d76d64187377f22b379a
Xavier <xpurcell@live.com>
2022-11-03 10:00:12 +1100

    Removed Twitter Tab

--- src/components/WebPortfolio/CreateProject.jsx
+++ src/components/WebPortfolio/CreateProject.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let CreateProject = (props) => {
   return (
     <div
@@ -28,34 +27,3 @@ let CreateProject = (props) => {
 };
 
 export default CreateProject;
-=======
-let CreateProject = (props) => {
-  return (
-    <div
-      style={{
-        backgroundColor: props.backgroundColor,
-        color: "white",
-        paddingTop: "50px",
-        paddingBottom: "50px",
-      }}
-    >
-      <div style={{ maxWidth: "800px", margin: "auto" }}>
-        <div>
-          <h2>Site: {props.titleInfo}</h2>
-          <h3>
-            URL: <a href={props.urlInfo}>{props.urlInfo}</a>
-          </h3>
-          <h4>Role: {props.roleInfo}</h4>
-          {props.extraInfo !== "" ? <h4>{props.extraInfo}</h4> : ""}
-        </div>
-        <img
-          src={props.projectImage}
-          style={{ maxWidth: "800px", marginTop: "50px" }}
-        ></img>
-      </div>
-    </div>
-  );
-};
-
-export default CreateProject;
->>>>>>> 0f4f1b07df421e0fb95242e2c2cd46e7352b252e
